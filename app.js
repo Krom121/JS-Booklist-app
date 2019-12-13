06e6fc26bb3835b78f712e5089324f837988247f
stephen <stephenmacgregor9@gmail.com>
2019-12-13 21:40:46 +0000

    validation/empty fields/add a book/delete a book

--- app.js
+++ app.js
@@ -50,6 +50,17 @@ class UI {
         }
     }
 
+    static showAlert(message, className){
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+        container.insertBefore(div, form);
+        // removes after 3 seconds
+        setTimeout(() => document.querySelector('.alert').remove(), 2500);
+    }
+
     static clearFields() {
         document.querySelector('#title').value = '';
         document.querySelector('#author').value = '';
@@ -74,18 +85,30 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
     const author = document.querySelector('#author').value;
     const isbn = document.querySelector('#isbn').value;
 
-    // Instatiate a book
-    const book = new Book(title, author, isbn);
+    // Validate
+    if(title === '' || author === '' || isbn === '') {
+       UI.showAlert('Please fill in all the fields', 'danger');
+    } else {
+        // Instatiate a book
+        const book = new Book(title, author, isbn);
 
-    // Add book to list
-    UI.addBookToList(book);
+        // Add book to list
+        UI.addBookToList(book);
 
-    // Clear fields
-    UI.clearFields();
+        // Successfully added a book alert
+        UI.showAlert('Your book has been added', 'success');
+
+        // Clear fields
+        UI.clearFields();
+    }
+
 });
 
 // Event to delete a book from the list
  document.querySelector('#book-list').addEventListener('click', (e) => {
     
-    UI.deleteBook(e.target)
+    UI.deleteBook(e.target);
+
+    // Successfully deleted a book alert
+     UI.showAlert('Your book has been deleted', 'success');
  });
